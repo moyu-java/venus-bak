73ddfd018d8af92848d8f2629b6b36df329d906f
James <moyubit@gmail.com>
2022-03-18 00:14:19 +0800

    feat: add venus-gateway module.

---
+++ venus-gateway/src/main/java/com/junmoyu/venus/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.junmoyu.venus.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * spring cloud gateway application.
+ *
+ * @author moyu.jun
+ * @date 2022/3/18
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
