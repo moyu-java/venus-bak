ab249ddbdf8592cd8da5d294faeb12ce6d4ead0c
James <moyubit@gmail.com>
2022-03-25 17:02:53 +0800

    feat: update dubbo.

--- venus-example/venus-example-dubbo/venus-example-dubbo-provider/src/main/java/com/junmoyu/venus/example/dubbo/provider/DubboProviderApplication.java
+++ venus-example/venus-example-dubbo/venus-example-dubbo-provider/src/main/java/com/junmoyu/venus/example/dubbo/provider/DubboProviderApplication.java
@@ -1,5 +1,6 @@
 package com.junmoyu.venus.example.dubbo.provider;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,8 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author moyu.jun
  * @date 2022/3/24
  */
+@EnableDubbo
 @SpringBootApplication
-@MapperScan("com.junmoyu.venus.example.single.boot.mapper")
+@MapperScan("com.junmoyu.venus.example.dubbo.provider.mapper")
 public class DubboProviderApplication {
     public static void main(String[] args) {
         SpringApplication.run(DubboProviderApplication.class, args);
