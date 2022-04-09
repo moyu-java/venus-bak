a78e4a123af5b37211d0f8f74e970baf013ef0c9
James <moyubit@gmail.com>
2022-04-09 20:08:22 +0800

    feat: add venus-auth module.

---
+++ venus-auth/src/main/java/com/junmoyu/venus/auth/AuthApplication.java
@@ -0,0 +1,17 @@
+package com.junmoyu.venus.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 认证中心启动器
+ *
+ * @author moyu.jun
+ * @date 2022/4/9
+ */
+@SpringBootApplication
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+}
